195d03685fbd1d23b4464adfbebec02e6b04c9ad
sbrevolution5 <sbrevolution5@aim.com>
2021-04-13 15:23:05 -0400

    font change, style update

--- js/site.js
+++ js/site.js
@@ -1,5 +1,13 @@
 function reverseString() {
     let userWord = document.getElementById("tacoCat").value;
+    let errorText = document.getElementById("errorText");
+    if(userWord == ""){
+        errorText.innerText ="Error: Please enter a word!";
+        return
+    }else{
+        errorText.innerText = ""
+    }
+
     let cleanWord = userWord.replace(/\W+/g, "").toLowerCase();
     // alert(userWord)
     // alert(cleanWord)
