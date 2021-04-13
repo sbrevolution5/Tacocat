896ca3d1ef99d89f4d0bf43e264873e4e86dccd7
sbrevolution5 <sbrevolution5@aim.com>
2021-04-13 11:38:21 -0400

    Updated titles, github link, and start of js

--- js/site.js
+++ js/site.js
@@ -1,10 +1,12 @@
 function reverseString() {
     let userWord = document.getElementById("tacoCat").value;
+    let cleanWord = userWord.replace(/\W+/g, "").toLowerCase();
     // alert(userWord)
-    let start = userWord.length-1;
+    alert(cleanWord)
+    let start = cleanWord.length-1;
     let newWord= ""
     for (let i = start; i >= 0; i--) {
-        newWord+= userWord[i]
+        newWord+= cleanWord[i]
     }
     let output= document.getElementById("flipped");
     output.innerText = newWord;
