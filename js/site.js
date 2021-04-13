235eb611efcfd700a046ecbd2ccd07000016de30
sbrevolution5 <sbrevolution5@aim.com>
2021-04-13 12:06:58 -0400

    added confetti

--- js/site.js
+++ js/site.js
@@ -2,7 +2,7 @@ function reverseString() {
     let userWord = document.getElementById("tacoCat").value;
     let cleanWord = userWord.replace(/\W+/g, "").toLowerCase();
     // alert(userWord)
-    alert(cleanWord)
+    // alert(cleanWord)
     let start = cleanWord.length-1;
     let newWord= ""
     for (let i = start; i >= 0; i--) {
@@ -10,4 +10,14 @@ function reverseString() {
     }
     let output= document.getElementById("flipped");
     output.innerText = newWord;
+    result = document.getElementById("yesOrNo")
+    if (newWord == cleanWord) {
+        confetti.start(1000)
+        
+        let yesAns = "Yes!!! &#128513; &#128513;"
+        result.innerHTML = yesAns
+    } else {
+        let noAns = "No &#128531;"
+        result.innerHTML = noAns
+    }
 }
